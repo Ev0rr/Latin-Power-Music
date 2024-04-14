e38ac6e3ecbdeb98f46ee576a6bde2890e24e28a
MR187078 <139504344+MR187078@users.noreply.github.com>
2024-04-13 20:00:55 -0600

    Add files via upload

---
+++ scr/app/crud-artists/crud-artists.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-artists',
+  standalone: true,
+  imports: [],
+  templateUrl: './crud-artists.component.html',
+  styleUrl: './crud-artists.component.css'
+})
+export class CrudArtistsComponent {
+
+}
