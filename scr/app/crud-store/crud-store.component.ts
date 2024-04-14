7d1f0f962c52191f8f4a6f15a8ebd7ec37d4973b
MR187078 <139504344+MR187078@users.noreply.github.com>
2024-04-13 20:08:16 -0600

    Add files via upload

---
+++ scr/app/crud-store/crud-store.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-store',
+  standalone: true,
+  imports: [],
+  templateUrl: './crud-store.component.html',
+  styleUrl: './crud-store.component.css'
+})
+export class CrudStoreComponent {
+
+}
