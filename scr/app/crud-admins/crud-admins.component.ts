0cc565954fbbda4cb58863a7bd38aafff4bdfa97
MR187078 <139504344+MR187078@users.noreply.github.com>
2024-04-13 19:42:34 -0600

    Add files via upload

---
+++ scr/app/crud-admins/crud-admins.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';z
+
+@Component({
+  selector: 'app-crud-admins',
+  standalone: true,
+  imports: [],
+  templateUrl: './crud-admins.component.html',
+  styleUrl: './crud-admins.component.css'
+})
+export class CrudAdminsComponent {
+
+}
