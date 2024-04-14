719741ad600e8d6ce7d67aee969728cd8cd43386
MR187078 <139504344+MR187078@users.noreply.github.com>
2024-04-13 20:06:44 -0600

    Add files via upload

---
+++ scr/app/crud-releases/crud-releases.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-releases',
+  standalone: true,
+  imports: [],
+  templateUrl: './crud-releases.component.html',
+  styleUrl: './crud-releases.component.css'
+})
+export class CrudReleasesComponent {
+
+}
