1d44427c4b7872ff803deded037988bc5f97504a
MR187078 <139504344+MR187078@users.noreply.github.com>
2024-04-13 20:04:47 -0600

    Add files via upload

---
+++ scr/app/crud-news/crud-news.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-news',
+  standalone: true,
+  imports: [],
+  templateUrl: './crud-news.component.html',
+  styleUrl: './crud-news.component.css'
+})
+export class CrudNewsComponent {
+
+}
