a19ee1ec6e0a0e7337a8041535a50586b11a581e
MR187078 <139504344+MR187078@users.noreply.github.com>
2024-04-13 20:09:45 -0600

    Add files via upload

---
+++ scr/app/crud-tickets/crud-tickets.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-tickets',
+  standalone: true,
+  imports: [],
+  templateUrl: './crud-tickets.component.html',
+  styleUrl: './crud-tickets.component.css'
+})
+export class CrudTicketsComponent {
+
+}
