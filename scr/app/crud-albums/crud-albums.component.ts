b2100a2aac907847ff2494e0fbff98646b4fcdda
MR187078 <139504344+MR187078@users.noreply.github.com>
2024-04-13 19:59:35 -0600

    Add files via upload

---
+++ scr/app/crud-albums/crud-albums.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-albums',
+  standalone: true,
+  imports: [],
+  templateUrl: './crud-albums.component.html',
+  styleUrl: './crud-albums.component.css'
+})
+export class CrudAlbumsComponent {
+
+}
