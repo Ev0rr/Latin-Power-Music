10d011eb543e10fa5f75d5ce9d35092365347cc9
MR187078 <139504344+MR187078@users.noreply.github.com>
2024-04-13 19:36:22 -0600

    Add files via upload

--- scr/app/app.component.ts
+++ scr/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, Inject, OnInit, PLATFORM_ID, ViewChild } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { StoreComponent } from "./store/store.component";
@@ -7,22 +7,95 @@ import { ArtistsComponent } from "./artists/artists.component";
 import { ReleasesComponent } from "./releases/releases.component";
 import { TicketsComponent } from "./tickets/tickets.component";
 import { CartComponent } from "./cart/cart.component";
-import { CommonModule } from '@angular/common';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { AuthService } from './services/auth.service';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule, HomeComponent, StoreComponent, AlbumsComponent, ArtistsComponent, ReleasesComponent, TicketsComponent, CartComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule, HomeComponent, StoreComponent, AlbumsComponent, ArtistsComponent, ReleasesComponent, TicketsComponent, CartComponent]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild('modal') modal!: ElementRef;
+
   title = 'Latin-Power-Music';
   showOptions = false;
+  isLoggedIn = false;
 
   toggleOptions() {
     console.log('toggleOptions called');
     this.showOptions = !this.showOptions;
     console.log('showOptions:', this.showOptions);
+  }
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object, public authService: AuthService, private cdr: ChangeDetectorRef) { }
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.authService.isLoggedIn = this.authService.isAuthenticated();
+      this.isLoggedIn = this.authService.isLoggedIn;
+      this.cdr.detectChanges();
+      const modal = document.getElementById("modal");
+      const hamburgerButton = document.querySelector(".hamburger-button");
+      const navLinks = document.querySelectorAll(".nav-link") as NodeListOf<HTMLElement>;
+      const loginLink = document.getElementById("loginLink");
+      const logoutLink = document.getElementById("logoutLink");
+
+      // Mostrar el modal cuando se hace clic en el botón de hamburguesa
+      if (hamburgerButton instanceof HTMLElement) {
+        hamburgerButton.addEventListener("click", () => {
+          if (modal) {
+            modal.style.display = "block";
+          }
+        });
+      }
+
+      // Ocultar el modal cuando se hace clic en cualquier enlace del navbar
+      Array.from(navLinks).forEach((link: HTMLElement) => {
+        link.addEventListener("click", () => {
+          if (modal) {
+            modal.style.display = "none";
+          }
+        });
+      });
+
+      // Ocultar el modal cuando se hace clic en el enlace de "Iniciar Sesión" dentro del modal
+      if (loginLink instanceof HTMLElement) {
+        loginLink.addEventListener("click", (event) => {
+          event.stopPropagation(); // Detener la propagación del evento
+          if (modal) {
+            modal.style.display = "none";
+          }
+        });
+      }
+
+      // Ocultar el modal cuando se hace clic en el enlace de "Cerrar Sesión" dentro del modal
+      if (logoutLink instanceof HTMLElement) {
+        logoutLink.addEventListener("click", (event) => {
+          event.stopPropagation(); // Detener la propagación del evento
+          if (modal) {
+            modal.style.display = "none";
+          }
+        });
+      }
+    }
+  }
+  logout() {
+    this.authService.logout(); // Llama a la función logout() del servicio AuthService
+  }
+
+  handleLoginClick(): void {
+    console.log('Iniciando sesión...');
+    // Aquí puedes agregar lógica adicional para iniciar sesión si es necesario
+    this.isLoggedIn = true;
+    
+  }
+
+  handleLogoutClick(): void {
+    console.log('Cerrando sesión...');
+    this.authService.logout();
+    this.isLoggedIn = false;
   }
 }
--- scr/app/app.routes.ts
+++ scr/app/app.routes.ts
@@ -9,6 +9,14 @@ import { TicketsComponent } from './tickets/tickets.component';
 import { CartComponent } from './cart/cart.component';
 import { LoginComponent } from './login/login.component';
 import { ControlPanelComponent } from './control-panel/control-panel.component';
+import { permissionsGuard } from './guards/permissions.guard';
+import { CrudAdminsComponent } from './crud-admins/crud-admins.component';
+import { CrudArtistsComponent } from './crud-artists/crud-artists.component';
+import { CrudAlbumsComponent } from './crud-albums/crud-albums.component';
+import { CrudReleasesComponent } from './crud-releases/crud-releases.component';
+import { CrudNewsComponent } from './crud-news/crud-news.component';
+import { CrudStoreComponent } from './crud-store/crud-store.component';
+import { CrudTicketsComponent } from './crud-tickets/crud-tickets.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
@@ -20,6 +28,13 @@ export const routes: Routes = [
     { path: 'tickets', component: TicketsComponent },
     { path: 'cart', component: CartComponent },
     { path: 'login', component: LoginComponent},
-    { path: 'control-panel', component: ControlPanelComponent},
+    { path: 'control-panel', component: ControlPanelComponent, canActivate: [permissionsGuard]},
+    { path: 'crud-admins', component: CrudAdminsComponent, canActivate: [permissionsGuard]},
+    { path: 'crud-artists', component: CrudArtistsComponent, canActivate: [permissionsGuard]},
+    { path: 'crud-albums', component: CrudAlbumsComponent, canActivate: [permissionsGuard]},
+    { path: 'crud-releases', component: CrudReleasesComponent, canActivate: [permissionsGuard]},
+    { path: 'crud-news', component: CrudNewsComponent, canActivate: [permissionsGuard]},
+    { path: 'crud-store', component: CrudStoreComponent, canActivate: [permissionsGuard]},
+    { path: 'crud-tickets', component: CrudTicketsComponent, canActivate: [permissionsGuard]},
     { path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
