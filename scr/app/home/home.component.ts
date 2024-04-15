53ea5c00dc46b6fd4571e2ea95bd4874a19df150
Dev-cuevas <118694793+Pedro-Cuevas-dev@users.noreply.github.com>
2024-04-15 02:57:26 -0600

    Update home.component.ts

--- scr/app/home/home.component.ts
+++ scr/app/home/home.component.ts
@@ -33,13 +33,15 @@ export class HomeComponent {
     return item.img;
   }
 
+  // Arreglo de álbumes con la URL de Spotify de cada artista
   albums = [
-    { img: "../../assets/images/el-mil-amores.png", alt: "Image 1" },
-    { img: "../../assets/images/linda-guerita.png", alt: "Image 2" },
-    { img: "../../assets/images/amantes-prohibidos.png", alt: "Image 3" },
-    { img: "../../assets/images/el-poder-de-la-musica.png", alt: "Image 4" },
-    { img: "../../assets/images/una-voz-que-hara-historia.png", alt: "Image 5" },
+    { artist: "Artista 1", img: "../../assets/images/el-mil-amores.png", alt: "Image 1", spotifyUrl: "https://open.spotify.com/intl-es/track/3d8GyDteAdYKO6IwuxCiOs?si=ceb5092aa1c94f34" },
+    { artist: "Artista 2", img: "../../assets/images/linda-guerita.png", alt: "Image 2", spotifyUrl: "https://open.spotify.com/intl-es/track/1NWvKwYuWuRrPUG9sEyoG5?si=9404022162124d12" },
+    { artist: "Artista 3", img: "../../assets/images/amantes-prohibidos.png", alt: "Image 3", spotifyUrl: "https://open.spotify.com/intl-es/track/21WYyyjovbDtf8Sa2DSdfZ?si=18fc284dc4fc409c" },
+    { artist: "Artista 4", img: "../../assets/images/el-poder-de-la-musica.png", alt: "Image 4", spotifyUrl: "https://open.spotify.com/intl-es/track/3ZyYM09zTUoUHeUqpqE2u9?si=44819c5a4fee4cfa" },
+    { artist: "Artista 5", img: "../../assets/images/una-voz-que-hara-historia.png", alt: "Image 5", spotifyUrl: "https://open.spotify.com/intl-es/track/3TVFdlCJzHc8rZrmpBEtdv?si=2df4b5fe9ab345bb" },
   ];
+
   albumConfig = {
     slidesToShow: 4,
     slidesToScroll: 1,
@@ -65,4 +67,9 @@ export class HomeComponent {
     prevArrow: '<button type="button" class="slick-prev">Previous</button>',
     nextArrow: '<button type="button" class="slick-next">Next</button>'
   };
+
+  // Función para redirigir a la canción en Spotify
+  redirectToSpotify(url: string) {
+    window.location.href = url;
+  }
 }
